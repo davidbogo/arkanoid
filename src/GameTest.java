928d0e55b65c9b4e5d10a456a3047213d2b6d27b
David Bogoslavsky <bogo3333@gmail.com>
2021-05-31 00:29:51 +0300

    Test file removed

--- src/GameTest.java
+++
@@ -1,7 +0,0 @@
-public class GameTest {
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.initialize(800, 600, 20, 100, 10);
-        game.run();
-    }
-}
